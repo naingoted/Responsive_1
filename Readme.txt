183a01aad44fccca8c7e1448134a1ab3f784f8cb
naingoted <boshan55@gmail.com>
2016-01-30 18:47:07 +0800

    version 1.0
    belated upload to git

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+@author Thet Naing Oo
+@title  tanintharyi site.
+
+There is already a design template which i had to reference.
+Responsive has been done by me.
+
+No sass nor compass has been used yet. but later project will be.
---
+++ site/js/custom.js
@@ -0,0 +1,11 @@
+
+
+<script>
+// Load this when the DOM is ready
+$(function(){
+  // You used .myCarousel here. 
+  // That's the class selector not the id selector,
+  // which is #myCarousel
+  $('#carousel-example-generic').carousel();
+});
+</script>
